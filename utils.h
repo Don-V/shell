965e18a8a69122be09760030d3c3ad6a9fcce0a1
Victor Amupitan <amupitan@iastate.edu>
2018-02-01 11:11:54 -0600

    Add util functions

---
+++ utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/**
+ * Trims leading and trailing white space from a char buffer
+ * \param str char buffer to be trimmed
+ * \return pointer to the first char of the buffer or 0 for NULL buffers
+ */
+char *trim(char *str);
+
+#endif
